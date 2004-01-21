39365a02584d83be6812c9c6ac1fbd4080b6a621
Jari Oksanen <jari.oksanen@oulu.fi>
2004-01-21 00:00:00 +0000

    version 1.6-2

--- R/prestondistr.R
+++ R/prestondistr.R
@@ -1,21 +1,25 @@
 "prestondistr" <-
-    function(x, ...)
+    function (x, truncate = -1,  ...) 
 {
-    fun <- function(par, x) {
+    fun <- function(par, x, truncate) {
         up <- dnorm(x, par[1], par[2], log = TRUE)
-        dn <- pnorm(0, par[1], par[2], lower=FALSE)
+        dn <- pnorm(truncate, par[1], par[2], lower = FALSE)
         -sum(up - log(dn))
     }
-    x <- x[x>0]
+    x <- x[x > 0]
     logx <- log2(x)
     p <- c(mean(logx), sd(logx))
-    sol <- optim(p, fun, x = logx)
+    sol <- optim(p, fun, x = logx, truncate = truncate)
     p <- sol$par
-    scale <- length(x)/sqrt(2*pi)/p[2]/pnorm(0, p[1], p[2], lower=FALSE)
+    area <- pnorm(truncate, p[1], p[2], lower = FALSE)
+    scale <- length(x)/sqrt(2 * pi)/p[2]/area
     p <- c(p, scale)
-    names(p) <- c("mode","width","S0")
-    out <- list(freq = as.preston(x), coefficients = p)
-    out$method <- "maximized likelihood to logarithmic abundances"
+    oct <- as.preston(x)
+    x <- as.numeric(names(oct))
+    fit <- p[3] * exp(-(x - p[1])^2/2/p[2]^2) 
+    names(p) <- c("mode", "width", "S0")
+    out <- list(freq = oct, fitted = fit, coefficients = p)
+    out$method <- "maximized likelihood to log2 abundances"
     class(out) <- "prestonfit"
     out
 }
