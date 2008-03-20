852dc3e21b3966bd8bad1c8e517205ffbc3d8513
Jari Oksanen <jari.oksanen@oulu.fi>
2008-03-20 16:09:08 +0000

    version 1.11-2

--- R/bstick.princomp.R
+++ R/bstick.princomp.R
@@ -4,7 +4,7 @@
     if(!inherits(n, "princomp"))
         stop("'n' not of class \"princomp\"")
     tot.chi <- sum(n$sdev^2)
-    n.comp <- n$n.obs
+    n.comp <- length(n$sdev)
     res <- bstick.default(n.comp, tot.chi, ...)
     names(res) <- dimnames(n$loadings)[[2]]
     res
