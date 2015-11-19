ddb389c570c253d48eb9d2eb504f6fc8d751c29c
Jari Oksanen <jari.oksanen@oulu.fi>
2015-11-19 17:20:34 +0000

    version 2.3-2

--- R/points.procrustes.R
+++ R/points.procrustes.R
@@ -1,19 +1,27 @@
 `points.procrustes` <-
-    function(x, display = c("target","rotated"), ...)
+    function(x, display = c("target","rotated"), choices = c(1,2),
+             truemean = FALSE, ...)
 {
     display <- match.arg(display)
-    x <- if (display == "target") x$X else x$Yrot
-    points(x, ...)
+    X <- if (display == "target") x$X else x$Yrot
+    X <- X[, choices, drop = FALSE]
+    if (truemean)
+        X <- sweep(X, 2, x$xmean[choices], "+")
+    ordiArgAbsorber(X, FUN = points, ...)
     invisible()
 }
 
 `text.procrustes` <-
-    function(x, display = c("target","rotated"), labels, ...)
+    function(x, display = c("target","rotated"), choices = c(1,2),
+             labels, truemean = FALSE, ...)
 {
     display <- match.arg(display)
-    x <- if (display == "target") x$X else x$Yrot
+    X <- if (display == "target") x$X else x$Yrot
+    X <- X[, choices, drop = FALSE]
+    if (truemean)
+        X <- sweep(X, 2, x$xmean[choices], "+")
     if (missing(labels))
-        labels <- rownames(x)
-    text(x, labels = labels, ...)
+        labels <- rownames(X)
+    ordiArgAbsorber(X, labels = labels, FUN = text, ...)
     invisible()
 }
