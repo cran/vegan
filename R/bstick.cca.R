c0c256860ac998efcac9d1ceca27118e5219a9ab
Jari Oksanen <jhoksane@gmail.com>
2018-10-25 07:00:03 +0000

    version 2.5-3

--- R/bstick.cca.R
+++ R/bstick.cca.R
@@ -5,10 +5,12 @@
         stop("'n' not of class \"cca\" or \"rda\"")
     if(!is.null(n$CCA) && n$CCA$rank > 0)
         stop("'bstick' only for unconstrained models")
-    ## No idea how to define bstick for capscale with negative
-    ## eigenvalues
-    if (inherits(n, "capscale") && !is.null(n$CA$imaginary.rank))
-        stop("'bstick' cannot be used for 'capscale' with imaginary component") 
+    ## No idea how to define bstick for dbrda or capscale with
+    ## negative eigenvalues
+    if (inherits(n, c("dbrda", "capscale")) &&
+        (!is.null(n$CA$imaginary.u) || !is.null(n$CA$imaginary.u.eig)))
+        stop(gettextf("'bstick' cannot be used for '%s' with negative eigenvalues",
+                      class(n)[1]))
     ## need to select appropriate total inertia
     tot.chi <- n$CA$tot.chi
     n.comp <- n$CA$rank
